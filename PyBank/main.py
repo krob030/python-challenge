747bb5b727fb3559e2a11e26bc403b6d67464ad6
krob030 <krob030@gmail.com>
2019-03-11 02:22:41 -0600

    bf

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import glob
 import os
-import csv
-csvpath= os.path.join("C:/Users/karsa_000/Desktop/Boot Camp/module 3/HWK/budget_data.csv")
-with open(csvpath, newline='') as csvfile:
-    csvreader= csv.reader(csvfile, delimiter=',')   
-    csv_header = next(csvreader)
-    #print(f"Header: {csv_header}")
-    Total_months = 0
-    for row in csvreader:
-      Total_months += 1
-    print (Total_months)
+
+
+# In[2]:
+
+
+csv_path= "C:/Users/karsa_000/Desktop/budget_data.csv"
+
+
+# In[14]:
+
+
+budget_pd= pd.read_csv(csv_path)
+
+
+# In[15]:
+
+
+budget_pd.columns = ["Date", "Profit_Loss"]
+
+
+# In[16]:
+
+
+TotMonths= budget_pd["Date"].count()
+
+
+# In[17]:
+
+
+Total= budget_pd["Profit_Loss"].sum()
+
+
+# In[18]:
+
+
+Avg= budget_pd["Profit_Loss"].mean()
+
+
+# In[19]:
+
+
+Great_Inc= budget_pd.max()
+
+
+# In[20]:
+
+
+Great_Dec= budget_pd.min()
+
+
+# In[21]:
+
+
+title= ("Financial Analysis")
+spaces= ("----------------------------")
+print(title)
+print(spaces)
+print("Total Months: " + str(TotMonths))
+print("Average  Change: " + str(Avg))
+print("Greatest Increase in Profits: " + str(Great_Inc.to_string(header= None, dtype=None)))
+print("Greatest Decrease in Profits: " + str(Great_Dec.to_string(header= None, dtype=None)))
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import pandas as pd
+import numpy as np
+import glob
 import os
-import csv
-csvpath= os.path.join("C:/Users/karsa_000/Desktop/Boot Camp/module 3/HWK/election_data.csv")
-with open(csvpath, newline='') as csvfile:
-    csvreader= csv.reader(csvfile, delimiter=',')   
-    csv_header = next(csvreader)
+
+
+# In[14]:
+
+
+csv_path= "C:/Users/karsa_000/Desktop/election_data.csv"
+
+
+# In[17]:
+
+
+election_pd= pd.read_csv(csv_path)
+
+
+# In[18]:
+
+
+election_pd.columns = ["Voter ID", "County", "Candidate"]
+
+
+# In[19]:
+
+
+TotVote= election_pd["Voter ID"].count()
+
+
+# In[20]:
+
+
+name= str(election_pd["Candidate"].unique())
+print(name)
+
+
+# In[21]:
+
+
+title= ("Election Results")
+spaces= ("----------------------------")
+print(title)
+print(spaces)
+print("Total Votes: " + str(TotVote))
+print(spaces)
+print(name)
+print(spaces)
+print(spaces)
+
