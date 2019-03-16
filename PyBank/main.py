64dd7903afa09711182c65b2bff163fbdaacdca2
krob030 <krob030@gmail.com>
2019-03-15 23:17:07 -0600

    2

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,13 @@ with open(csvpath, newline='') as csvfile:
 with open(csvpath, newline='') as csvfile:
     csvreader= csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+    column_sum = 0
+    for line in csvreader:
+        column_sum += int(line[1])
+    #print(column_sum)
+with open(csvpath, newline='') as csvfile:
+    csvreader= csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
     # extract first line
     first_line = next(csvreader)
     # init previous value
@@ -35,7 +42,7 @@ with open(csvpath, newline='') as csvfile:
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: "+ str(Total_months))
-print("Total: $")
+print("Total: $"+ str(column_sum))
 print("Average  Change: $")
 print("Greatest Increase in Profits: "+ "($" + str(GI)+ ")")
 print("Greatest Decrease in Profits: "+ "($" + str(GD)+ ")")
@@ -43,7 +50,7 @@ text_file = open("Output.txt", "w")
 text_file.write("Financial Analysis"+ '\n')
 text_file.write("----------------------------"+ '\n')
 text_file.write("Total Months: "+ str(Total_months)+ '\n')
-text_file.write("Total: $"+ '\n')
+text_file.write("Total: $"+ str(column_sum)+ '\n')
 text_file.write("Average  Change: $"+ '\n')
 text_file.write("Greatest Increase in Profits: "+ "($" + str(GI)+ ")"+ '\n')
 text_file.write("Greatest Decrease in Profits: "+ "($" + str(GD)+ ")"+ '\n')
